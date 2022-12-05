ca0a485ce44a5499b78ee5e0f30f038d2f2dde95
dpirkl <david.pirkl@stud.tu-darmstadt.de>
2022-12-05 22:37:27 +0100

    added a delete method, automatically deletes files without further use, and changed formatting

--- utils/file_handler.py
+++ utils/file_handler.py
@@ -1,3 +1,11 @@
+"""This module contains functions for handling files.
+This includes:
+- splitting a video into audio and video files
+- merging audio and video files
+- deleting files
+- merging video and captions
+"""
+
 import subprocess
 import os
 
@@ -8,9 +16,10 @@ def get_audio_from_video_file(
     video_file: str, output_path: str = AUDIO_DIRECTORY, file_name: str = None
 ) -> None:
     """Extracts the audio from the given video file and saves it to the audio directory."""
-    file_name = file_name if file_name else os.path.basename(video_file).split(".")[0]
 
+    file_name = file_name if file_name else os.path.basename(video_file).split(".")[0]
     audio_path = output_path / file_name + ".wav"
+
     command = f"ffmpeg -y -i {video_file} -ab 160k -ac 2 -ar 44100 -vn {audio_path}"
     subprocess.call(command, shell=True)
 
@@ -26,10 +35,14 @@ def merge_audio_and_video_to_mp4(
     The output path is without the filename or extension."""
 
     file_name = file_name if file_name else os.path.basename(video_file).split(".")[0]
-
     output_path = output_path / file_name + ".mp4"
+
     command = f"ffmpeg -y -i {video_file} -i {audio_file} -c:v copy -c:a aac -strict experimental -b:a 192k {output_path}"
     subprocess.call(command, shell=True)
+
+    # Delete the video and audio file.
+    delete(video_file)
+    delete(audio_file)
 
 
 def remove_audio_from_video_file(
@@ -39,8 +52,8 @@ def remove_audio_from_video_file(
     The output path is without the filename or extension."""
 
     file_name = file_name if file_name else os.path.basename(video_file).split(".")[0]
-
     output_path = output_path / file_name + ".mp4"
+
     command = f"ffmpeg -y -i {str(video_file)} -vcodec copy -an {output_path}"
     subprocess.call(command, shell=True)
 
@@ -52,9 +65,40 @@ def merge_video_and_captions(
     file_name: str = None,
 ) -> None:
     """Merges a video file and captions file and saves it to the destination.
-    The output path is without the filename or extension."""
-    file_name = file_name if file_name else os.path.basename(video_file).split(".")[0]
+    The output path is without the filename or extension.
+    The file name is without the extension."""
 
+    file_name = file_name if file_name else os.path.basename(video_file).split(".")[0]
     output_path = output_path / file_name + ".mp4"
+
     command = f"ffmpeg -y -i {video_file} -vf subtitles={captions_file} {output_path}"
     subprocess.call(command, shell=True)
+
+    # Delete the video and captions file
+    delete(video_file)
+    delete(captions_file)
+
+
+def split_video(
+    video_file: str,
+    video_output_path: str = str(VIDEO_DIRECTORY),
+    video_file_name: str = None,
+    audio_output_path: str = str(AUDIO_DIRECTORY),
+    audio_file_name: str = None,
+) -> None:
+    """Splits the given video file into an audio file and a video file without audio."""
+
+    get_audio_from_video_file(
+        video_file=video_file, output_path=audio_output_path, file_name=audio_file_name
+    )
+    remove_audio_from_video_file(
+        video_file=video_file, output_path=video_output_path, file_name=video_file_name
+    )
+
+    # Delete the video file
+    delete(video_file)
+
+
+def delete(file_path: str) -> None:
+    """Deletes the file at the given path."""
+    os.remove(file_path)
