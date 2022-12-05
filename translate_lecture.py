5699e1bb411e5d5268d69689332f866167a4c035
dpirkl <david.pirkl@stud.tu-darmstadt.de>
2022-12-05 14:25:47 +0100

    main file to translate the lecture

---
+++ translate_lecture.py
@@ -0,0 +1,81 @@
+"""This module is used to translate the videos."""
+import whisper
+
+from src.tts_wrapper import Speaker
+
+from utils import file_handler
+from utils import (
+    AUDIO_DEST_DIRECTORY,
+    AUDIO_DIRECTORY,
+    VIDEO_DIRECTORY,
+    VIDEO_DEST_DIRECTORY,
+    ORIGINAL_VIDEO_DIRECTORY,
+    TRANSCRIPT_DIRECTORY,
+    CAPTIONS_DIRECTORY,
+)
+
+
+def main():
+    """This function is the main function of the program. It is called when the program is executed.
+    It is responsible for the whole process of translating a lecture.
+    Steps:
+    - split all videos into audio files and video files without audio
+    - translate and transcribe the audio files
+    - generate captions and text files
+    - synthesize the audio files
+    - merge the audio files and the video files
+    - merge the video files and the captions
+    """
+
+    # split all videos into audio files and video files without audio
+    # audio files are saved in AUDIO_DIRECTORY
+    # new video files are saved in VIDEO_DIRECTORY
+    for original_video in ORIGINAL_VIDEO_DIRECTORY.iterdir():
+        file_handler.get_audio_from_video_file(original_video)
+        file_handler.remove_audio_from_video_file(original_video)
+
+    # load the model
+    model = whisper.load_model("tiny")
+    options = {"task": "translate", "fp16": False, "beam_size": 5, "best_of": 5}
+
+    # load the synthesizer
+    synthesizer = Speaker()
+
+    for audio_file in AUDIO_DIRECTORY.iterdir():
+        # transcribe audio file
+        lecture_name = audio_file.stem
+        result = model.transcribe(audio_file, **options)
+
+        # save transcript to a text file
+        transcript_path = TRANSCRIPT_DIRECTORY / f"{lecture_name}.txt"
+        with open(transcript_path, "w") as f:
+            whisper.utils.write_txt(result, file=f)
+
+        # generate captions
+        captions_path_vtt = CAPTIONS_DIRECTORY / f"{lecture_name}.vtt"
+        with open(captions_path_vtt, "w") as vvt:
+            whisper.utils.write_vtt(result["segments"], file=vvt)
+
+        captions_path_srt = CAPTIONS_DIRECTORY / f"{lecture_name}.srt"
+        with open(captions_path_srt, "w") as srt:
+            whisper.utils.write_srt(result["segments"], file=srt)
+
+        # synthesize audio file
+        translated_audio_path = AUDIO_DEST_DIRECTORY / f"{lecture_name}.wav"
+        synthesizer.speak(result["text"], translated_audio_path)
+
+    for audio_file, video_file in zip(
+        AUDIO_DIRECTORY.iterdir(), VIDEO_DIRECTORY.iterdir()
+    ):
+        # merge audio file and video file
+        file_handler.merge_audio_and_video_to_mp4(video_file, audio_file)
+
+    for video_file in VIDEO_DEST_DIRECTORY.iterdir:
+        # add captions to video file
+        video_name = video_file.stem
+        captions_file = f"{video_name}.vtt"
+        file_handler.merge_video_and_captions(video_file, captions_file)
+
+
+if __name__ == "__main__":
+    main()
