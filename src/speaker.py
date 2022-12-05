2228b3fcddf6a9b14e1572667c9ae642d47d60e9
dpirkl <david.pirkl@stud.tu-darmstadt.de>
2022-12-05 22:46:39 +0100

    changes

--- src/speaker.py
+++
@@ -1,9 +0,0 @@
-import abc
-from abc import ABC
-
-
-class Speaker(ABC):
-
-    @abc.abstractmethod
-    def save_to_file(self, text: str, filename: str, audio_format: str = "mp3") -> None:
-        pass
--- tests/test.py
+++
@@ -1,7 +0,0 @@
-from pathlib import Path
-import run_without_deepl
-
-path = Path(__file__).parent.parent / "data" / "audio" / "test.mp3"
-
-print(path)
-run_without_deepl.main()
