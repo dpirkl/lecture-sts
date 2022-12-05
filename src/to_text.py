a9264c62424eae9493d053c4e1857597d23fcd42
dpirkl <david.pirkl@stud.tu-darmstadt.de>
2022-12-05 14:40:41 +0100

    reformated and added method descriptions

--- src/to_text.py
+++ src/to_text.py
@@ -2,21 +2,17 @@ import whisper
 
 
 class SpeechToText:
-    """A class for transcribing audio files.
-
-    """
+    """A class for transcribing audio files."""
 
     model: whisper.Whisper
     fp16_settings: bool
 
-    def __init__(self, model: str = 'small', fp16_settings: bool = False):
+    def __init__(self, model: str = "small", fp16_settings: bool = False):
         self.model = whisper.load_model(model)
         self.fp16_settings = fp16_settings
 
     def transcribe(self, file: str) -> str:
-        """Transcribes the given audio file.
-
-        """
+        """Transcribes the given audio file."""
         options = whisper.DecodingOptions(fp16=self.fp16_settings)
 
         result = self.model.transcribe(file, fp16=self.fp16_settings)["text"]
@@ -24,13 +20,13 @@ class SpeechToText:
         return result
 
     def transcribe_and_translate(self, file: str) -> str:
-        """Transcribes and translates the given audio file.
+        """Transcribes and translates the given audio file."""
 
-        """
+        options = whisper.DecodingOptions(task="translate")
 
-        options = whisper.DecodingOptions(task='translate')
-
-        result = self.model.transcribe(file, fp16=self.fp16_settings, options=options)["text"]
+        result = self.model.transcribe(file, fp16=self.fp16_settings, options=options)[
+            "text"
+        ]
 
         return result
 
@@ -62,7 +58,7 @@ class SpeechToText:
 
         mel = whisper.log_mel_spectrogram(audio).to(self.model.device)
 
-        options = whisper.DecodingOptions(fp16=self.fp16_settings, task='translate')
+        options = whisper.DecodingOptions(fp16=self.fp16_settings, task="translate")
         result = whisper.decode(self.model, mel, options).text
 
         return result
