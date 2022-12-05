0d040ec2216b8bc70dddf155faa638dc057b85ec
dpirkl <david.pirkl@stud.tu-darmstadt.de>
2022-12-05 21:58:15 +0100

    fixed bugs

--- utils/file_handler.py
+++ utils/file_handler.py
@@ -1,4 +1,5 @@
 import subprocess
+import os
 
 from pathlib import Path
 
@@ -14,12 +15,8 @@ from utils import (
 
 def get_audio_from_video_file(video_file: str) -> None:
     """Extracts the audio from the given video file and saves it to the audio directory."""
-    print(video_file)
-    video_path: Path = video_file
-    video_name = video_path.name.split(".")[0]
-    print(video_name)
+    video_name = os.path.basename(video_file).split(".")[0]
     audio_path = AUDIO_DIRECTORY / (video_name + ".wav")
-    print(audio_path)
     command = f"ffmpeg -y -i {video_file} -ab 160k -ac 2 -ar 44100 -vn {audio_path}"
     subprocess.call(command, shell=True)
 
@@ -28,18 +25,20 @@ def merge_audio_and_video_to_mp4(video_file: str, audio_file: str) -> None:
     """Fuses the audio (from AUDIO_DIRECTORY) and video (from VIDEO_DIRECTORY)
     file and saves it to the video destination (VIDEO_DEST_DIRECTORY) directory."""
 
-    video_path = VIDEO_DIRECTORY / video_file
-    audio_path = AUDIO_DEST_DIRECTORY / audio_file
-    output_path = VIDEO_DEST_DIRECTORY / (video_file.split(".")[0] + ".mp4")
-    command = f"ffmpeg -y -i {video_path} -i {audio_path} -c copy -map 0:v:0 -map 1:a:0 {output_path}"
+    # merge video and mono audio
+
+    output_path = VIDEO_DEST_DIRECTORY / (
+        os.path.basename(video_file).split(".")[0] + ".mp4"
+    )
+    command = f"ffmpeg -y -i {video_file} -i {audio_file} -c:v copy -c:a aac -strict experimental -b:a 192k {output_path}"
     subprocess.call(command, shell=True)
 
 
 def remove_audio_from_video_file(video_file: str) -> None:
     """Removes the audio from the given video (from VIDEO_DIRECTORY) file and saves it to the video destination directory."""
-    video_path = ORIGINAL_VIDEO_DIRECTORY / video_file
-    output_path = VIDEO_DIRECTORY / video_file
-    command = f"ffmpeg -y -i {video_path} -vcodec copy -an {output_path}"
+    video_name = os.path.basename(video_file).split(".")[0]
+    output_path = VIDEO_DIRECTORY / f"{video_name}.mp4"
+    command = f"ffmpeg -y -i {str(video_file)} -vcodec copy -an {output_path}"
     subprocess.call(command, shell=True)
 
 
@@ -47,8 +46,7 @@ def merge_video_and_captions(video_file: str, captions_file: str) -> None:
     """Merges the video (from VIDEO_DIRECTORY) and captions (from CAPTIONS_DIRECTORY)
     file and saves it to the video destination (VIDEO_DEST_DIRECTORY) directory."""
 
-    video_path = VIDEO_DIRECTORY / video_file
-    captions_path = CAPTIONS_DIRECTORY / captions_file
-    output_path = VIDEO_DEST_DIRECTORY / video_file
-    command = f"ffmpeg -y -i {video_path} -vf subtitles={captions_path} {output_path}"
+    video_name = os.path.basename(video_file).split(".")[0]
+    output_path = VIDEO_DEST_DIRECTORY / f"{video_name}.mp4"
+    command = f"ffmpeg -y -i {video_file} -vf subtitles={captions_file} {output_path}"
     subprocess.call(command, shell=True)
