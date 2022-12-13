2a27815d8176e5602eb2b04e832e01c7a0b39737
dpirkl <david.pirkl@stud.tu-darmstadt.de>
2022-12-13 16:39:51 +0100

    changed setup.py

--- setup.py
+++ setup.py
@@ -1,13 +1,4 @@
-import whisper
-
-from src import tts_wrapper
 from utils import path_handler
 
 # Create folders
 path_handler.create_folders()
-
-# Download TTS models
-tts_wrapper.download_models()
-
-# Download Whisper models
-whisper.load_model("large")
