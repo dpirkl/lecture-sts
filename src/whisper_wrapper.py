72131bf379d65a86fd71a186db6d50b08406366d
dpirkl <david.pirkl@stud.tu-darmstadt.de>
2022-12-17 01:00:21 +0100

    simpified

--- src/whisper_wrapper.py
+++ src/whisper_wrapper.py
@@ -29,36 +29,3 @@ class SpeechToText:
         ]
 
         return result
-
-    def transcribe_advanced(self, file: str) -> str:
-        """Transcribes the given audio file. Uses more steps than transcribe() for an optimized transcription.
-        Only the first 30 seconds of the audio file will be transcribed.
-
-        """
-
-        # Processing of the audio file. Trims or pads it to fit 30 seconds.
-        audio = whisper.load_audio(file)
-        audio = whisper.pad_or_trim(audio)
-
-        mel = whisper.log_mel_spectrogram(audio).to(self.model.device)
-
-        options = whisper.DecodingOptions(fp16=self.fp16_settings)
-        result = whisper.decode(self.model, mel, options).text
-
-        return result
-
-    def transcribe_and_translate_advanced(self, file: str) -> str:
-        """Transcribes and translates the given audio file.
-        Only the first 30 seconds of the audio file will be transcribed and translated.
-
-        """
-
-        audio = whisper.load_audio(file)
-        audio = whisper.pad_or_trim(audio)
-
-        mel = whisper.log_mel_spectrogram(audio).to(self.model.device)
-
-        options = whisper.DecodingOptions(fp16=self.fp16_settings, task="translate")
-        result = whisper.decode(self.model, mel, options).text
-
-        return result
