63ca67218c61220dacf2cb6534a6261e4f27b0d4
dpirkl <david.pirkl@stud.tu-darmstadt.de>
2022-12-17 03:55:11 +0100

    changed the name of the class and added comments

--- src/whisper_wrapper.py
+++ src/whisper_wrapper.py
@@ -1,30 +1,45 @@
+"""This class is a simple wrapper for the whisper libary"""
 import whisper
 
 
-class SpeechToText:
-    """A class for transcribing audio files."""
+class Transcriber:
 
     model: whisper.Whisper
     fp16_settings: bool
 
     def __init__(self, model: str = "small", fp16_settings: bool = False):
-        self.model = whisper.load_model(model)
+        """Initializes a Transcriber object. You can set the model size and specify the fp16 settings.
+
+
+        Args:
+            model (str, optional): The size of the transcription model. Defaults to "small".
+            fp16_settings (bool, optional): Wheter to use fp16 (or fp32). Defaults to False.
+        """
+
+        self.model = whisper.load_model(name=model)
         self.fp16_settings = fp16_settings
 
-    def transcribe(self, file: str) -> str:
-        """Transcribes the given audio file."""
+    def transcribe(self, file: str) -> dict:
+        """This method transcribes a given audio file.
 
-        result = self.model.transcribe(file, fp16=self.fp16_settings)["text"]
+        Args:
+            file (str): The path to the audio file.
 
-        return result
+        Returns:
+            dict: The result of the transcripton. The plain text can be accessed by 'result["text"]'.
+        """
 
-    def transcribe_and_translate(self, file: str) -> str:
-        """Transcribes and translates the given audio file."""
+        return self.model.transcribe(file, fp16=self.fp16_settings)
+
+    def transcribe_and_translate(self, file: str) -> dict:
+        """This method transcribes the audio file and translates it to english.
+
+        Args:
+            file (str): The path to the audio file.
+
+        Returns:
+            dict: The result of the transcription. The plain text can be accessed by 'result["text"]'.
+        """
 
         options = whisper.DecodingOptions(task="translate")
-
-        result = self.model.transcribe(file, fp16=self.fp16_settings, options=options)[
-            "text"
-        ]
-
-        return result
+        return self.model.transcribe(file, fp16=self.fp16_settings, options=options)
